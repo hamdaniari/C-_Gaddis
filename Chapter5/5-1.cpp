170fecec61e98c637a7933eb8295d0252038be4a
Ari Hamdani <aryham04@gmail.com>
2020-08-19 16:20:49 +0900

    increment ++

---
+++ Chapter5/5-1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int num = 4;
+  num++;
+  cout << "num++ : " << num << endl;
+  ++num;
+  cout << "++num :" << num << endl;
+  
+  return 0;
+}
